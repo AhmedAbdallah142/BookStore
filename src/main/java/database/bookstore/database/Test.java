71814b132a1cfaf8d0394cdd0dc8f3437ec3b9f1
Ahmed4Elfax <ahmed55fax@gmail.com>
2022-01-09 00:53:18 +0200

    test

---
+++ src/main/java/database/bookstore/database/Test.java
@@ -0,0 +1,13 @@
+package database.bookstore.database;
+
+import java.sql.SQLException;
+
+public class Test {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		CustomerDatabase c = new CustomerDatabase();
+		c.insertBook(null);
+	}
+
+}
