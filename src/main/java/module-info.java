2250e6429dce35182d2bd2d31e3c43f82f8236e1
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-01-08 11:23:09 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module database.bookstore {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens database.bookstore to javafx.fxml;
+    exports database.bookstore;
+}
