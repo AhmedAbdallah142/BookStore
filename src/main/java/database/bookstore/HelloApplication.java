c1ec97601566c93582918a9ba22ab4306954d950
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-01-08 11:51:36 +0200

    set background

--- src/main/java/database/bookstore/HelloApplication.java
+++ src/main/java/database/bookstore/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("UserSign.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1300, 700);
+        stage.setTitle("User Sign");
         stage.setScene(scene);
         stage.show();
     }
