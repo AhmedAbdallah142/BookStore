fa61020094d4dc10fafb491e3395b0fd5257d51f
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-01-09 21:12:55 +0200

    add & remove books attribute

--- src/main/java/database/bookstore/viewController/AdminPanelController.java
+++ src/main/java/database/bookstore/viewController/AdminPanelController.java
@@ -5,7 +5,26 @@ import javafx.scene.control.TextField;
 
 public class AdminPanelController {
     @FXML
+    private TextField addIsbn;
+    @FXML
+    private TextField publisherName;
+    @FXML
+    private TextField title;
+    @FXML
+    private TextField publicationYear;
+    @FXML
+    private TextField price;
+    @FXML
+    private TextField category;
+    @FXML
+    private TextField threshold;
+    @FXML
+    private TextField noCopies;
+    @FXML
     private TextField authors;
+    @FXML
+    private TextField removeIsbn;
+
     @FXML
     protected void onAddClick(){
 
