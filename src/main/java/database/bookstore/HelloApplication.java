12528dc31c535e9ac9378d82f6a2b64f2f199ba4
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-01-09 16:51:28 +0200

    home page

--- src/main/java/database/bookstore/HelloApplication.java
+++ src/main/java/database/bookstore/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("UserLogIn.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Log In...!");
         stage.setScene(scene);
