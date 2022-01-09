afc3256b5215a880102de5f2749cc5474c43652f
Ahmed4Elfax <ahmed55fax@gmail.com>
2022-01-10 01:41:56 +0200

    2

--- src/main/java/database/bookstore/database/OrderDatabase.java
+++ src/main/java/database/bookstore/database/OrderDatabase.java
@@ -1,5 +1,14 @@
 package database.bookstore.database;
 
-public class OrderDatabase {
+import database.bookstore.entites.Order;
 
+public class OrderDatabase {
+	
+	public void place_order(Order o) {
+		
+	}
+
+	public void confirm_order(int id) {
+		
+	}
 }
--- src/main/java/database/bookstore/entites/Order.java
+++ src/main/java/database/bookstore/entites/Order.java
@@ -1,21 +1,12 @@
 package database.bookstore.entites;
 
 public class Order {
-
-	int order_id;
 	int ISBN;
 	int quantity;
-	public Order(int order_id, int iSBN, int quantity) {
+	public Order(int iSBN, int quantity) {
 		super();
-		this.order_id = order_id;
 		ISBN = iSBN;
 		this.quantity = quantity;
-	}
-	public int getOrder_id() {
-		return order_id;
-	}
-	public void setOrder_id(int order_id) {
-		this.order_id = order_id;
 	}
 	public int getISBN() {
 		return ISBN;
