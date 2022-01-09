ee11eede2173037dd546b4279888983b56e3430d
mark74384 <58529681+mark74384@users.noreply.github.com>
2022-01-09 02:26:15 +0200

    insert book

--- src/main/java/database/bookstore/database/CustomerDatabase.java
+++ src/main/java/database/bookstore/database/CustomerDatabase.java
@@ -13,7 +13,27 @@ public class CustomerDatabase {
     }
 
     public void insertBook(Book b) throws SQLException {
-    	dataBase.getStatement().execute("");
+    	dataBase.getStatement().execute("INSERT INTO Book VALUES"
+                + "( "
+                + b.getISBN() + ","
+                + "'" + b.getTitle() + "'" + ","
+                + "'" + b.getPublisher() + "'" + ","
+                + "'" + b.getPublication_year() + "'" + ","
+                + b.getPrice() +","
+                + "'" + b.getCategory() + "'" + ","
+                + b.getCopies() + ","
+                + b.getThreshold()
+                +");"
+        );
+        for (String authorName : b.getAuthors()){
+            dataBase.getStatement().execute("INSERT INTO Author VALUES"
+                    + "("
+                    + "'" + authorName + "'" + ","
+                    + b.getISBN()
+                    + "):"
+            );
+        }
+
     }
     
     
