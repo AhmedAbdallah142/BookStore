827e79a9a960723fadf81f5373eeae93e60045b7
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-01-09 18:59:46 +0200

    Cart & reports

--- src/main/java/database/bookstore/HelloApplication.java
+++ src/main/java/database/bookstore/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Cart.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Log In...!");
         stage.setScene(scene);
---
+++ src/main/java/database/bookstore/viewController/Cart.java
@@ -0,0 +1,4 @@
+package database.bookstore.viewController;
+
+public class Cart {
+}
