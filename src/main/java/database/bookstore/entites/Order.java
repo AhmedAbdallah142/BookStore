9cd770378910afa67e8f739f7b9dfb41c5906245
Ahmed4Elfax <ahmed55fax@gmail.com>
2022-01-10 13:59:23 +0200

    order

--- src/main/java/database/bookstore/entites/Order.java
+++ src/main/java/database/bookstore/entites/Order.java
@@ -1,10 +1,18 @@
 package database.bookstore.entites;
 
 public class Order {
+	int ID;
 	int ISBN;
 	int quantity;
 	public Order(int iSBN, int quantity) {
 		super();
+		ISBN = iSBN;
+		this.quantity = quantity;
+	}
+	
+	public Order(int iD, int iSBN, int quantity) {
+		super();
+		ID = iD;
 		ISBN = iSBN;
 		this.quantity = quantity;
 	}
@@ -24,6 +32,14 @@ public class Order {
 	}
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
 	}
 	
 }
