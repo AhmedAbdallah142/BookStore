ee1efbf591e1d645465f5d889f4473df0bb9f7e8
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-01-10 16:26:09 +0200

    reports

--- src/main/java/database/bookstore/viewController/ReportsController.java
+++ src/main/java/database/bookstore/viewController/ReportsController.java
@@ -1,12 +1,16 @@
 package database.bookstore.viewController;
 
+import database.bookstore.database.ReportDatabase;
+import database.bookstore.entites.ReportEmailItem;
 import database.bookstore.entites.ReportSaleItem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+
 
 public class ReportsController {
     @FXML
@@ -14,12 +18,54 @@ public class ReportsController {
     @FXML
     private TableView<ReportSaleItem> totalSales;
     @FXML
-    private TableColumn<ReportSaleItem,String> tssotalSales;
+    private TableColumn<ReportSaleItem, Integer> totalSalesISBN;
+    @FXML
+    private TableColumn<ReportSaleItem, String> totalSalesTitle;
+    @FXML
+    private TableColumn<ReportSaleItem, Integer> totalSalesQuantity;
+    @FXML
+    private TableView<ReportEmailItem> top5;
+    @FXML
+    private TableColumn<ReportEmailItem, Integer> top5Email;
+    @FXML
+    private TableColumn<ReportEmailItem, Double> top5Purchases;
+    @FXML
+    private TableView<ReportSaleItem> top10;
+    @FXML
+    private TableColumn<ReportSaleItem, String> top10ISBN;
+    @FXML
+    private TableColumn<ReportSaleItem, String> top10Title;
+    @FXML
+    private TableColumn<ReportSaleItem, String> top10Quantity;
+
+    @FXML
+    public void initialize() {
+        try {
+            totalSalesISBN.setCellValueFactory(new PropertyValueFactory<>("ISBN"));
+            totalSalesTitle.setCellValueFactory(new PropertyValueFactory<>("Title"));
+            totalSalesQuantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+            top5Email.setCellValueFactory(new PropertyValueFactory<>("Email"));
+            top5Purchases.setCellValueFactory(new PropertyValueFactory<>("Total_purchases"));
+            top10ISBN.setCellValueFactory(new PropertyValueFactory<>("ISBN"));
+            top10Title.setCellValueFactory(new PropertyValueFactory<>("Title"));
+            top10Quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+            ReportDatabase r = new ReportDatabase();
+            totalSales.getItems().addAll(r.Total_Sales_For_Books(Integer.parseInt(Page.getText())));
+            top5.getItems().addAll(r.Top_Customers_last_three(1));
+            top10.getItems().addAll(r.Top_Sales_For_Books_last_three(1));
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+
     @FXML
     protected void onLeftArrowClick() {
         try {
             int page = Integer.parseInt(Page.getText());
-            Page.setText(String.valueOf(page == 1 ? 1:page-1));
+            if (page != 1){
+                Page.setText(String.valueOf(page - 1));
+            }
         } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
             alert.showAndWait();
@@ -30,7 +76,7 @@ public class ReportsController {
     protected void onRightArrowClick() {
         try {
             int page = Integer.parseInt(Page.getText());
-            Page.setText(String.valueOf(page+1));
+            Page.setText(String.valueOf(page + 1));
         } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
             alert.showAndWait();
